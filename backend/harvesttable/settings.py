17c4e12d8099fe717932829482cc056affe20c19
Gummadala vikas <vikas93912@gmail.com>
2026-03-11 19:49:37 +0530

    env added

--- backend/harvesttable/settings.py
+++ backend/harvesttable/settings.py
@@ -47,13 +47,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'harvesttable.urls'
 
-EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST         = 'smtp.gmail.com'        
-EMAIL_PORT         = 587
-EMAIL_USE_TLS      = True
-EMAIL_HOST_USER    = 'vikas93912@gmail.com'
-EMAIL_HOST_PASSWORD = 'qibp gjrb pmee mnek'
-DEFAULT_FROM_EMAIL = 'HarvestTable <noreply@harvesttable.com>'
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 CONTACT_STAFF_EMAIL = 'support@harvesttable.com'
 
